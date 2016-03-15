06f516c12894aca8478bfa214216b45f2e40fa8c
woshijpf <woshijpfgg@163.com>
2016-03-16 00:20:15 +0800

    学习第二章中引用和标识符相关内容

---
+++ charpter2/exercise_2_12.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	// 下面两个标志符号可以编译通过
+	int _;
+	double Double = 3.14;
+	return 0;
+}
---
+++ charpter2/exercise_2_16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+
+	int i = 0, &r1 = i;
+	double d = 0, &r2 = d;
+
+	r2 = 3.1415926;
+	std::cout << "r2 = " << r2 << " d = " << d << std::endl;
+	r2 = r1;
+	std::cout << "r2 = " << r2 << " d = " << d << std::endl;
+	i = r2;
+	std::cout << "r1 = " << r1 << " i = " << i << std::endl;
+	r1 = d;
+	std::cout << "r1 = " << r1 << " i = " << i << std::endl;
+
+	return 0;
+}
